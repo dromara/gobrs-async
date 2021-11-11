38ee76c0c9781cdfa264cfc6688fde3e9a5b49d9
wuweifeng10 <wuweifeng10@jd.com>
2021-11-11 11:02:02 +0800

    1

---
+++ src/test/java/parallel/ParWorker7.java
@@ -0,0 +1,53 @@
+package parallel;
+
+
+import com.jd.platform.async.callback.ICallback;
+import com.jd.platform.async.callback.IWorker;
+import com.jd.platform.async.executor.timer.SystemClock;
+import com.jd.platform.async.worker.WorkResult;
+import com.jd.platform.async.wrapper.WorkerWrapper;
+
+import java.util.Map;
+
+/**
+ * @author wuweifeng wrote on 2019-11-20.
+ */
+public class ParWorker7 implements IWorker<String, String>, ICallback<String, String> {
+    private long sleepTime = 1000;
+
+    public void setSleepTime(long sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+    @Override
+    public String action(String object, Map<String, WorkerWrapper> allWrappers) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "result = " + SystemClock.now() + "---param = " + object + " from 3";
+    }
+
+
+    @Override
+    public String defaultValue() {
+        return "worker3--default";
+    }
+
+    @Override
+    public void begin() {
+        //System.out.println(Thread.currentThread().getName() + "- start --" + System.currentTimeMillis());
+    }
+
+    @Override
+    public void result(boolean success, String param, WorkResult<String> workResult) {
+        if (success) {
+            System.out.println("callback worker3 success--" + SystemClock.now() + "----" + workResult.getResult()
+                    + "-threadName:" +Thread.currentThread().getName());
+        } else {
+            System.err.println("callback worker3 failure--" + SystemClock.now() + "----"  + workResult.getResult()
+                    + "-threadName:" +Thread.currentThread().getName());
+        }
+    }
+
+}
