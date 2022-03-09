26c26a41e3741894570beb656f5175dbe72f0a0d
sizegang1 <sizegang1@jd.com>
2022-03-09 21:17:40 +0800

    threadpoolconfig

--- gobrs-async-example/src/main/java/com/jd/gobrs/async/example/config/ThreadPoolConfig.java
+++ gobrs-async-example/src/main/java/com/jd/gobrs/async/example/config/ThreadPoolConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import javax.annotation.PostConstruct;
+import java.util.concurrent.*;
 
 /**
  * @program: gobrs-async
@@ -23,23 +23,12 @@ public class ThreadPoolConfig {
     @Autowired
     private GobrsAsyncThreadPoolFactory factory;
 
-    private ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
 
-    @Bean
-    public ThreadPoolTaskExecutor gobrsThreadPoolExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setBeanName("jd-thead-pool");
-        executor.setCorePoolSize(1000);//配置核心线程数
-        executor.setMaxPoolSize(2000);//配置最大线程数
-        executor.setKeepAliveSeconds(30);
-        executor.setQueueCapacity(10000);//配置队列大小
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());//拒绝策略
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.initialize();//执行初始化
-
+    @PostConstruct
+    public void gobrsThreadPoolExecutor(){
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(300, 500, 30, TimeUnit.SECONDS,
+                new LinkedBlockingQueue());
         factory.setThreadPoolExecutor(threadPoolExecutor);
-        return executor;
     }
-
 
 }
