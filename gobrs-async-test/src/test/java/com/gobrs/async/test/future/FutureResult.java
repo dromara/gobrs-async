63f9cf145ee3e88ae0c8a694601b48987349abf0
sizegang1 <sizegang1@jd.com>
2022-11-09 14:47:16 +0800

    Future gets the return result of a non-dependent task

--- gobrs-async-test/src/test/java/com/gobrs/async/test/future/FutureResult.java
+++ gobrs-async-test/src/test/java/com/gobrs/async/test/future/FutureResult.java
@@ -10,28 +10,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @program: gobrs-async
- * @ClassName CaseOne
+ * The type Future result.
+ *
+ * @program: gobrs -async
+ * @ClassName FutureResult
  * @description:
  * @author: sizegang
- * @create: 2022-10-31
- **/
+ * @create: 2022 -10-31
+ */
 @SpringBootTest(classes = GobrsAsyncTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FutureResult {
-
-    /**
-     * A -> B,C,D
-     */
 
     @Autowired
     private GobrsAsync gobrsAsync;
 
-
+    /**
+     * 获取非强依赖任务的返回结果  如下： C任务获取A任务的返回结果， 从任务配置上看 C并不需要A 执行完成后再执行C 所有 通过Future方式 C有能力获取到A的返回结果
+     * Future task rule.
+     * - name: "futureTaskRule"
+     *   content: "futureTaskA->futureTaskB;futureTaskC->futureTaskD"
+     *   task-interrupt: true # 局部异常是否打断主流程 默认 false
+     */
     @Test
-    public void caseOne() {
+    public void futureTaskRule() {
         Map<String, Object> params = new HashMap<>();
         gobrsAsync.go("futureTaskRule", () -> params);
-
     }
 
 }
