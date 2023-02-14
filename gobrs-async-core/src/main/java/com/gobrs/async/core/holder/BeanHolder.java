b2cb5f95a584f5f661299ebb31e9f7cacf34f31d
sizegang1 <sizegang1@jd.com>
2023-02-14 15:24:56 +0800

    修复 无插件配置无法启动问题

--- gobrs-async-core/src/main/java/com/gobrs/async/core/holder/BeanHolder.java
+++ gobrs-async-core/src/main/java/com/gobrs/async/core/holder/BeanHolder.java
@@ -79,11 +79,16 @@ public class BeanHolder implements ApplicationContextAware, BeanFactoryPostProce
      * @return the bean
      */
     public static <T> T getBean(Class<T> clazz) {
-        ApplicationContext applicationContext = getApplicationContext();
-        if (applicationContext == null) {
-            return cf.getBean(clazz);
+        try {
+            ApplicationContext applicationContext = getApplicationContext();
+            if (applicationContext == null) {
+                return cf.getBean(clazz);
+            }
+            return applicationContext.getBean(clazz);
+        } catch (Exception exception) {
+            logger.warn(" class {} getBean error{}", clazz, exception);
+            return null;
         }
-        return applicationContext.getBean(clazz);
     }
 
     /**
